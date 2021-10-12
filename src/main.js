4a1d0b403bc7a50d5d425d1f6e8f1ea791199b46
nazzal ramzan <nazzalra@gmail.com>
2021-10-12 08:41:14 +0700

    import bootstrap jquery popperjs and app.scss

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import jQuery from "jquery";
+import "@popperjs/core";
+import "bootstrap";
+import "@/assets/app.scss";
 
+window.$ = window.jQuery = jQuery;
 Vue.config.productionTip = false;
 
-new Vue({
+new Vue( {
   router,
-  render: (h) => h(App),
-}).$mount("#app");
+  render: ( h ) => h( App ),
+} ).$mount( "#app" );
