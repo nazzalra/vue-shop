1a677ca3540809265d0b2a2ece9e8b2c45e18c89
nazzal ramzan <nazzalra@gmail.com>
2021-10-13 17:32:59 +0700

    create hero component for Home page

---
+++ src/components/Hero.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hero">
+    <div class="container">
+      <div class="row justify-content-center align-items-center">
+        <div class="col-md-5">
+          <h1 class="hero-title">Vue Shop for Developer</h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident unde architecto nisi
+            aliquid ipsam dicta atque nemo nostrum laborum quam iusto dolorem harum, libero soluta,
+            ad, maxime adipisci nesciunt laboriosam.
+          </p>
+          <div class="hero-btn mt-4">
+            <button class="btn btn-info px-4 py-2">Explore</button>
+            <button class="btn btn-outline-secondary ml-4 px-4 py-2">Products</button>
+          </div>
+        </div>
+        <div class="col-md-7">
+          <img src="/img/svg/graphic.svg" alt="" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss" scoped>
+.hero {
+  padding-top: 7rem;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="home">
     <Navbar />
+    <Hero />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import Hero from "@/components/Hero";
 
 export default {
   name: "Home",
-  components: {},
+  components: {
+    Hero,
+  },
 };
 </script>
