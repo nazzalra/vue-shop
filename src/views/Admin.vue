12db765f8d2f53c48c35c1bf4ead1a877d09eef3
nazzal ramzan <nazzalra@gmail.com>
2021-10-13 14:07:32 +0700

    setup admin page

---
+++ src/views/Admin.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="admin">
+    <div class="page-wrapper default-theme sidebar-bg bg1 toggled">
+      <div class="menu-show position-absolute p-3" @click="toggleMenu">
+        <i class="fas fa-bars menu-show-icon"></i>
+      </div>
+      <nav id="sidebar" class="sidebar-wrapper">
+        <div class="sidebar-content">
+          <!-- sidebar-brand  -->
+          <div class="sidebar-item sidebar-brand">
+            <a href="#">Vue Shop</a>
+            <div class="sidebar-icon" @click="toggleMenu">
+              <i class="fas fa-times sidebar-icon-close"></i>
+            </div>
+          </div>
+          <!-- sidebar-header  -->
+          <div class="sidebar-item sidebar-header d-flex flex-nowrap">
+            <div class="user-pic">
+              <img
+                class="img-responsive img-rounded"
+                :src="`${publicPath}img/user.png`"
+                alt="User picture"
+              />
+            </div>
+            <div class="user-info">
+              <span class="user-name"
+                >Jhon
+                <strong>Smith</strong>
+              </span>
+              <span class="user-role">Administrator</span>
+              <span class="user-status">
+                <i class="fa fa-circle"></i>
+                <span>Online</span>
+              </span>
+            </div>
+          </div>
+          <!-- sidebar-search  -->
+          <div class="sidebar-item sidebar-search">
+            <div>
+              <div class="input-group">
+                <input type="text" class="form-control search-menu" placeholder="Search..." />
+                <div class="input-group-append">
+                  <span class="input-group-text">
+                    <i class="fa fa-search" aria-hidden="true"></i>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- sidebar-menu  -->
+          <div class="sidebar-item sidebar-menu">
+            <ul>
+              <li class="header-menu">
+                <span>Menu</span>
+              </li>
+              <li>
+                <a href="#">
+                  <i class="fas fa-chart-line"></i>
+                  <span class="menu-text">Overview</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i class="fab fa-amazon"></i>
+                  <span class="menu-text">Products</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i class="fas fa-shopping-cart"></i>
+                  <span class="menu-text">Orders</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i class="fas fa-power-off"></i>
+                  <span class="menu-text">Logout</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <!-- sidebar-menu  -->
+        </div>
+      </nav>
+      <!-- page-content  -->
+      <main class="page-content pt-2">
+        <div id="overlay" class="overlay"></div>
+        <div class="container-fluid p-5">
+          <div class="row">
+            <div class="form-group col-md-12">
+              <h2>Pro Sidebar</h2>
+              <p>
+                This is a responsive sidebar template with dropdown menu based on bootstrap
+                framework.
+              </p>
+            </div>
+          </div>
+        </div>
+      </main>
+      <!-- page-content" -->
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Admin",
+  components: {},
+  methods: {
+    toggleMenu() {
+      $(".page-wrapper").toggleClass("toggled");
+    },
+  },
+  data() {
+    return {
+      publicPath: process.env.BASE_URL,
+    };
+  },
+};
+</script>
+<style lang="scss" scoped>
+.menu-show {
+  cursor: pointer;
+  &-icon {
+    font-size: 1.1rem;
+  }
+}
+</style>
